15ed0f0a7e5954adb0f6d41362d25ce67874f263
Wout Schellaert <wout.schellaert@gmail.com>
2023-11-02 11:52:55 +0100

    Add clean train and test runs

--- scripts/slurmintegrationtest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sbatch <<EOT
-#!/bin/bash
-#SBATCH --gpus=0
-#SBATCH --job-name="integration-test"
-#SBATCH --output=X%j-%x.out
-singularity --nv dreamerv3-animalai.sif python tests/integration.py
-EOT
