1c7ece4d2338b5555c6ee0483f7c01f792576423
mgm21 <mgm18@ic.ac.uk>
2024-06-11 18:12:12 +0100

    Correct python training script name in shell script

--- recurrent_ppo/scripts/competition-curriculum-timescale300.sh
+++ recurrent_ppo/scripts/competition-curriculum-timescale300.sh
@@ -1,11 +1,11 @@
-  xvfb-run -a python train.py \
+  xvfb-run -a python train_from_sh.py \
   --task ./aai/configs/paper/curriculumL1.yaml \
   --env ./aai/env/env3.1.3/AAI.x86_64 \
   --logdir ./logdir/competition-curriculum/competition-curriculum-timescale300-L1 \
   --timesteps 2000000 \
   --aai_timescale 300
 
-   xvfb-run -a python train.py \
+   xvfb-run -a python train_from_sh.py \
    --task ./aai/configs/paper/curriculumL1_2.yaml \
    --env ./aai/env/env3.1.3/AAI.x86_64 \
    --from-checkpoint ./logdir/competition-curriculum/competition-curriculum-timescale300-L1/checkpoint.ckpt \
@@ -13,7 +13,7 @@
    --timesteps 2000000 \
    --aai_timescale 300
 
-  xvfb-run -a python train.py \
+  xvfb-run -a python train_from_sh.py \
    --task ./aai/configs/paper/curriculumL1_3.yaml \
    --env ./aai/env/env3.1.3/AAI.x86_64 \
    --from-checkpoint ./logdir/competition-curriculum/competition-curriculum-timescale300-L1_2/checkpoint.ckpt \
@@ -21,7 +21,7 @@
    --number_steps 2000000 \
    --aai_timescale 300
 
-  xvfb-run -a python train.py \
+  xvfb-run -a python train_from_sh.py \
    --task ./aai/configs/paper/curriculumL1_4.yaml \
    --env ./aai/env/env3.1.3/AAI.x86_64 \
    --from-checkpoint ./logdir/competition-curriculum/competition-curriculum-timescale300-L1_3/checkpoint.ckpt \
@@ -29,7 +29,7 @@
    --timesteps 2000000  \
    --aai_timescale 300
 
-  xvfb-run -a python train.py \
+  xvfb-run -a python train_from_sh.py \
    --task ./aai/configs/paper/curriculumL1_5.yaml \
    --env ./aai/env/env3.1.3/AAI.x86_64 \
    --from-checkpoint ./logdir/competition-curriculum/competition-curriculum-timescale300-L1_4/checkpoint.ckpt \
@@ -37,7 +37,7 @@
    --timesteps 2000000 \
    --aai_timescale 300
 
-  xvfb-run -a python train.py \
+  xvfb-run -a python train_from_sh.py \
    --task ./aai/configs/paper/curriculumL1_6.yaml \
    --env ./aai/env/env3.1.3/AAI.x86_64 \
    --from-checkpoint ./logdir/competition-curriculum/competition-curriculum-timescale300-L1_5/checkpoint.ckpt \
@@ -45,7 +45,7 @@
    --timesteps 2000000 \
    --aai_timescale 300
 
-  xvfb-run -a python train.py \
+  xvfb-run -a python train_from_sh.py \
    --task ./aai/configs/paper/curriculumL1_7.yaml \
    --env ./aai/env/env3.1.3/AAI.x86_64 \
    --from-checkpoint ./logdir/competition-curriculum/competition-curriculum-timescale300-L1_6/checkpoint.ckpt \
@@ -53,7 +53,7 @@
    --timesteps 2000000 \
    --aai_timescale 300
 
-  xvfb-run -a python train.py \
+  xvfb-run -a python train_from_sh.py \
    --task ./aai/configs/paper/curriculumL1_8.yaml \
    --env ./aai/env/env3.1.3/AAI.x86_64 \
    --from-checkpoint ./logdir/competition-curriculum/competition-curriculum-timescale300-L1_7/checkpoint.ckpt \
@@ -61,7 +61,7 @@
    --timesteps 2000000 \
    --aai_timescale 300
 
-  xvfb-run -a python train.py \
+  xvfb-run -a python train_from_sh.py \
    --task ./aai/configs/paper/curriculumL1_9.yaml \
    --env ./aai/env/env3.1.3/AAI.x86_64 \
    --from-checkpoint ./logdir/competition-curriculum/competition-curriculum-timescale300-L1_8/checkpoint.ckpt \
@@ -69,7 +69,7 @@
    --timesteps 2000000 \
    --aai_timescale 300
 
-  xvfb-run -a python train.py \
+  xvfb-run -a python train_from_sh.py \
    --task ./aai/configs/paper/curriculumL1_10.yaml \
    --env ./aai/env/env3.1.3/AAI.x86_64 \
    --from-checkpoint ./logdir/competition-curriculum/competition-curriculum-timescale300-L1_9/checkpoint.ckpt \
@@ -77,7 +77,7 @@
    --timesteps 2000000 \
    --aai_timescale 300
 
- xvfb-run -a python train.py \
+ xvfb-run -a python train_from_sh.py \
   --task ./aai/configs/paper/curriculumL1_10.yaml \
   --env ./aai/env/env3.1.3/AAI.x86_64 \
   --from-checkpoint ./logdir/competition-curriculum/competition-curriculum-timescale300-L1_10/checkpoint.ckpt \
