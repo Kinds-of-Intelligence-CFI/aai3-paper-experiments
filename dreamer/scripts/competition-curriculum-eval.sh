32ec1f24dd830bb26b7c2406f7551caa786372d4
unknown <k.voudouris14@googlemail.com>
2024-06-11 14:32:56 +0100

    Update shell script with cuda visible devices

--- dreamer/scripts/competition-curriculum-eval.sh
+++ dreamer/scripts/competition-curriculum-eval.sh
@@ -1,4 +1,4 @@
-xvfb-run -a python train.py \
+CUDA_VISIBLE_DEVICES=1 xvfb-run -a python train.py \
   --task ./aai/configs/paper/competition1 \
   --env ./aai/env/env3.1.3/AAI.x86_64 \
   --logdir ./logdir/competition-curriculum/competition-curriculum-eval-ind1 \
@@ -6,7 +6,7 @@ xvfb-run -a python train.py \
   --eval-mode \
   --dreamer-args "--run.steps 900000"
 
-xvfb-run -a python train.py \
+CUDA_VISIBLE_DEVICES=1 xvfb-run -a python train.py \
   --task ./aai/configs/paper/competition2 \
   --env ./aai/env/env3.1.3/AAI.x86_64 \
   --logdir ./logdir/competition-curriculum/competition-curriculum-eval-ind2 \
@@ -14,7 +14,7 @@ xvfb-run -a python train.py \
   --eval-mode \
   --dreamer-args "--run.steps 900000"
 
-xvfb-run -a python train.py \
+CUDA_VISIBLE_DEVICES=1 xvfb-run -a python train.py \
   --task ./aai/configs/paper/competition3 \
   --env ./aai/env/env3.1.3/AAI.x86_64 \
   --logdir ./logdir/competition-curriculum/competition-curriculum-eval-ind3 \
