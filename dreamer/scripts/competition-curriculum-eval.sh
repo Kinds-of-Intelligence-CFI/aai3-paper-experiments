8875fd8a9ec5c74f2449605518cbf1a1c93b35e3
unknown <k.voudouris14@googlemail.com>
2024-06-11 14:29:22 +0100

    Add eval script for curriculum dreamer agent

---
+++ dreamer/scripts/competition-curriculum-eval.sh
@@ -0,0 +1,23 @@
+xvfb-run -a python train.py \
+  --task ./aai/configs/paper/competition1 \
+  --env ./aai/env/env3.1.3/AAI.x86_64 \
+  --logdir ./logdir/competition-curriculum/competition-curriculum-eval-ind1 \
+  --from-checkpoint ./logdir/competition-curriculum/competition-curriculum-timescale300-L1_10_5M/checkpoint.ckpt \
+  --eval-mode \
+  --dreamer-args "--run.steps 900000"
+
+xvfb-run -a python train.py \
+  --task ./aai/configs/paper/competition2 \
+  --env ./aai/env/env3.1.3/AAI.x86_64 \
+  --logdir ./logdir/competition-curriculum/competition-curriculum-eval-ind2 \
+  --from-checkpoint ./logdir/competition-curriculum/competition-curriculum-timescale300-L1_10_5M/checkpoint.ckpt \
+  --eval-mode \
+  --dreamer-args "--run.steps 900000"
+
+xvfb-run -a python train.py \
+  --task ./aai/configs/paper/competition3 \
+  --env ./aai/env/env3.1.3/AAI.x86_64 \
+  --logdir ./logdir/competition-curriculum/competition-curriculum-eval-ind3 \
+  --from-checkpoint ./logdir/competition-curriculum/competition-curriculum-timescale300-L1_10_5M/checkpoint.ckpt \
+  --eval-mode \
+  --dreamer-args "--run.steps 900000"
