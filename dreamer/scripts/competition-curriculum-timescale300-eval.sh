5bf2d5ec0203af4639603700fa63928590f7fa3a
kozzy97 <k.voudouris14@googlemail.com>
2024-06-14 15:57:45 +0100

    Run extra competition configs

--- dreamer/scripts/competition-curriculum-timescale300-eval.sh
+++ dreamer/scripts/competition-curriculum-timescale300-eval.sh
@@ -24,3 +24,12 @@ CUDA_VISIBLE_DEVICES=1 xvfb-run -a python train.py \
   --eval-mode \
   --dreamer-args "--run.steps 300000" \
   --aai-timescale 300
+
+#CUDA_VISIBLE_DEVICES=1 xvfb-run -a python train.py \
+#  --task ./aai/configs/paper/competition-extra \
+#  --env ./aai/env/env3.1.3/AAI.x86_64 \
+#  --logdir ./logdir/competition-curriculum/competition-curriculum-timescale300-eval-extra_3 \
+#  --from-checkpoint ./logdir/competition-curriculum/competition-curriculum-timescale300-L1_10_5M/checkpoint.ckpt \
+#  --eval-mode \
+#  --dreamer-args "--run.steps 90000" \
+#  --aai-timescale 300
