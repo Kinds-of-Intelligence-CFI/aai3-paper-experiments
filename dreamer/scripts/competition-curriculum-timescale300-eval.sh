b3bb5b792a988bd977eaf5bd7d070fc08f9b4917
kozzy97 <k.voudouris14@googlemail.com>
2024-06-18 17:08:09 +0100

    Add missing data

--- dreamer/scripts/competition-curriculum-timescale300-eval.sh
+++ dreamer/scripts/competition-curriculum-timescale300-eval.sh
@@ -28,8 +28,8 @@ CUDA_VISIBLE_DEVICES=1 xvfb-run -a python train.py \
 #CUDA_VISIBLE_DEVICES=1 xvfb-run -a python train.py \
 #  --task ./aai/configs/paper/competition-extra \
 #  --env ./aai/env/env3.1.3/AAI.x86_64 \
-#  --logdir ./logdir/competition-curriculum/competition-curriculum-timescale300-eval-extra_3 \
+#  --logdir ./logdir/competition-curriculum/competition-curriculum-timescale300-eval-extra \
 #  --from-checkpoint ./logdir/competition-curriculum/competition-curriculum-timescale300-L1_10_5M/checkpoint.ckpt \
 #  --eval-mode \
-#  --dreamer-args "--run.steps 90000" \
+#  --dreamer-args "--run.steps 64000" \
 #  --aai-timescale 300
