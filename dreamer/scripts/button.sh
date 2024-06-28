834860ac3dad264ade5c577131382bb62f9193f4
unknown <k.voudouris14@googlemail.com>
2024-06-28 09:42:25 +0100

    Remove old button experiments

--- dreamer/scripts/button.sh
+++
@@ -1,5 +0,0 @@
-xvfb-run -a python train.py \
-  --task ./aai/configs/paper/buttonPressGreenCurriculum.yml \
-  --env ./aai/env/env3.1.3/AAI.x86_64 \
-  --logdir ./logdir/button-train \
-  --dreamer-args "--run.steps 2000000"
