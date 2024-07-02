1faec3a99646e4cab16779efed3596f6841a3a42
kozzy97 <k.voudouris14@googlemail.com>
2024-07-02 12:03:31 +0100

    Fix path in foraging eval script

--- dreamer/scripts/foragingTask/foraging-eval.sh
+++ dreamer/scripts/foragingTask/foraging-eval.sh
@@ -2,7 +2,7 @@ xvfb-run -a python train.py \
   --task ../configs/foragingTask/foragingTaskSpawnerTree.yml \
   --env ../env/AAI.x86_64 \
   --logdir ./logdir/foraging/foraging-eval \
-  --from-checkpoint ./logdir/foraging-train/checkpoint.ckpt \
+  --from-checkpoint ./logdir/foraging/foraging-train/checkpoint.ckpt \
   --eval-mode \
   --dreamer-args "--run.steps 500000"
 # 500k steps is at least 100 eval episodes at max 3k steps per episode
