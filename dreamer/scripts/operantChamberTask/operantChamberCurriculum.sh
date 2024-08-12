7b1c2f085bc6b952d5037c0c2c37fa46b2f86718
kozzy97 <k.voudouris14@googlemail.com>
2024-08-12 09:33:40 +0200

    Fix operant chamber curriculum

--- dreamer/scripts/operantChamberTask/operantChamberCurriculum.sh
+++ dreamer/scripts/operantChamberTask/operantChamberCurriculum.sh
@@ -9,25 +9,25 @@ xvfb-run -a python train.py \
   --env ../env/AAI.x86_64 \
   --from-checkpoint ./logdir/operantChamber/operantChamber-A-train/checkpoint.ckpt \
   --logdir ./logdir/operantChamber/operantChamber-B-train \
-  --dreamer-args "--run.steps 400000"
+  --dreamer-args "--run.steps 800000"
 
 xvfb-run -a python train.py \
   --task ../configs/operantChamberTask/operantChamberTaskCurriculum-C.yml \
   --env ../env/AAI.x86_64 \
   --from-checkpoint ./logdir/operantChamber/operantChamber-B-train/checkpoint.ckpt \
   --logdir ./logdir/operantChamber/operantChamber-C-train \
-  --dreamer-args "--run.steps 400000"
+  --dreamer-args "--run.steps 1200000"
 
 xvfb-run -a python train.py \
   --task ../configs/operantChamberTask/operantChamberTaskCurriculum-D.yml \
   --env ../env/AAI.x86_64 \
   --from-checkpoint ./logdir/operantChamber/operantChamber-C-train/checkpoint.ckpt \
   --logdir ./logdir/operantChamber/operantChamber-D-train \
-  --dreamer-args "--run.steps 400000"
+  --dreamer-args "--run.steps 1600000"
 
 xvfb-run -a python train.py \
   --task ../configs/operantChamberTask/operantChamberTaskCurriculum-E.yml \
   --env ../env/AAI.x86_64 \
   --from-checkpoint ./logdir/operantChamber/operantChamber-D-train/checkpoint.ckpt \
   --logdir ./logdir/operantChamber/operantChamber-E-train \
-  --dreamer-args "--run.steps 400000"
+  --dreamer-args "--run.steps 2000000"
