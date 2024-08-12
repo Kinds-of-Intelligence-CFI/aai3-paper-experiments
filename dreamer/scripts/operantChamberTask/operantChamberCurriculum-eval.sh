28b36ae271ee4e093e93741593637522ae51edf6
kozzy97 <k.voudouris14@googlemail.com>
2024-08-12 09:44:53 +0200

    Update path for operant chamber curriculum evaluation

--- dreamer/scripts/operantChamberTask/operantChamberCurriculum-eval.sh
+++ dreamer/scripts/operantChamberTask/operantChamberCurriculum-eval.sh
@@ -1,7 +1,7 @@
 xvfb-run -a python train.py \
   --task ../configs/operantChamberTask/operantChamberTask.yml \
   --env ../env/AAI.x86_64 \
-  --logdir ./logdir/operantChamber/operantChamber-eval \
+  --logdir ./logdir/operantChamber/operantChamberCurriculum-eval \
   --from-checkpoint ./logdir/operantChamber/operantChamber-E-train/checkpoint.ckpt \
   --eval-mode \
   --dreamer-args "--run.steps 500000"
